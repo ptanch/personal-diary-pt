1076e22f80f7d19bd25c63298f138f1d1a472a53
Tatiana Pitieva <tatyane.liss@gmail.com>
2026-01-12 21:23:12 +0300

    создана админ панель для модели diary

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from diary.models import Diary
+
+
+@admin.register(Diary)
+class DiaryAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "title", "is_private", "created_at", "updated_at")
+    list_filter = ("is_private", "created_at")
+    ordering = ("-created_at",)
+    date_hierarchy = "created_at"
+    readonly_fields = ("created_at", "updated_at")
