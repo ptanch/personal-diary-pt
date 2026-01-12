65e4b93976c8bc676875849370f39cc50d7bb035
Tatiana Pitieva <tatyane.liss@gmail.com>
2026-01-12 21:23:44 +0300

    создана админ панель для модели user

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    model = User
+    ordering = ("email",)
+    list_display = ("id", "email", "first_name", "last_name", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "sex")
+    search_fields = ("email", "first_name", "last_name", "phone")
