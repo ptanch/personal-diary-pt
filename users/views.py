7f7fb22a3d1d6caf034c1e974f6ebebb42897dbd
Tatiana Pitieva <tatyane.liss@gmail.com>
2026-01-16 00:03:02 +0300

    добавлены контроллеры для работы с пользователем: создание профиля, вход в учетку

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import LoginForm, UserRegisterForm
+from .models import User
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = "users/register.html"
+    success_url = reverse_lazy("diary:diary_list")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        messages.success(self.request, "Аккаунт создан! Добро пожаловать 👋")
+        return response
+
+
+class UserLoginView(LoginView):
+    form_class = LoginForm
+    template_name = "users/login.html"
