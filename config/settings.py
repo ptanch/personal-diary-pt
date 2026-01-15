73790e1ec485ef0716fa2176a49cc07ce58bffd6
Tatiana Pitieva <tatyane.liss@gmail.com>
2026-01-16 00:04:09 +0300

    добавлены настройки логики редиректа после login

--- config/settings.py
+++ config/settings.py
@@ -104,3 +104,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+LOGIN_REDIRECT_URL = "diary:diary_list"
+LOGOUT_REDIRECT_URL = "users:login"
+LOGIN_URL = "users:login"
