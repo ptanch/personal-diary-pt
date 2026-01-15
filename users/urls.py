488440df3e00c41d73a0b6f79ead764146fc1efd
Tatiana Pitieva <tatyane.liss@gmail.com>
2026-01-16 00:05:38 +0300

    добавлены маршруты для работы с пользователями: регистрация, вход в учетку, выход

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from users.views import UserCreateView, UserLoginView
+
+app_name = "users"
+
+urlpatterns = [
+    path("register/", UserCreateView.as_view(), name="register"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="users:login"), name="logout"),
+]
