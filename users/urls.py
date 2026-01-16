355a9b572e252ca1080abf87243e49430a7abec4
Tatiana Pitieva <tatyane.liss@gmail.com>
2026-01-16 23:05:33 +0300

    добавлен путь до шаблона с профилем пользователя

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from users.views import UserCreateView, UserLoginView
+from users.views import UserCreateView, UserLoginView, UserProfileView
 
 app_name = "users"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("register/", UserCreateView.as_view(), name="register"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="users:login"), name="logout"),
+    path("profile/", UserProfileView.as_view(), name="profile")
 ]
