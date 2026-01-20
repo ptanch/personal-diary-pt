ae42f78aaf67034e01c8eb85f1449b8a202f236f
Tatiana Pitieva <tatyane.liss@gmail.com>
2026-01-20 18:25:58 +0300

    добавлены настройки хранения статических материлов

--- config/settings.py
+++ config/settings.py
@@ -97,7 +97,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
